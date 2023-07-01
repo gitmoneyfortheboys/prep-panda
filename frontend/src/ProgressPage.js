9564a91051b9193e9a206bd07a9ff048e1de00d3
Hadi Abou-Ghaida <hdghaida@gmail.com>
2023-07-01 11:01:07 +1000

    Fixed colour scheme of progress page.

--- frontend/src/ProgressPage.js
+++ frontend/src/ProgressPage.js
@@ -16,16 +16,16 @@ const CardContainer = styled.div`
 `;
 
 const Card = styled.div`
-  background: #fff;
+  background: ${props => props.background || "#fff"}; // Add this line
   border-radius: 10px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 270px;
+  height: 300px;
   margin: 1rem;
   padding: 20px;
-  width: 270px;
+  width: 300px;
   box-shadow: 0px 4px 10px rgba(0,0,0,0.1); // Shadow effect
   transition: 0.3s; // Transition effect for hover
   
@@ -38,47 +38,51 @@ const Card = styled.div`
 
 
 const Title = styled.h2`
-  color: #3f51b5;
-  margin-bottom: 20px; // Add some margin at the bottom
+  color: ${props => props.textColor || "#3f51b5"};
+  margin-bottom: 20px;
 `;
 
 const Value = styled.h2`
-  color: #3f51b5;
+  color: ${props => props.textColor || "#3f51b5"};
   font-size: 24px;
   font-weight: bold;
 `;
 
 
-function ProgressPage() {
-  const data = [{name: 'Math', value: 300},{name: 'Reading', value: 200}, {name: 'Thinking', value: 100}];
 
+function ProgressPage() {
+  const data = [{name: 'Math', value: 30},{name: 'Reading', value: 10}, {name: 'Thinking', value: 20}];
+
+  // Array of colors for the cards
+  const cardColors = ["#9FD725", "#EFEFEF", "#3454D1", "#070707", "#3874ffba", "#E09F3E"];
+  
   return (
     <div>
       <h1>Here's your progress so far:</h1>
       <CardContainer>
-      <Card>
-        <Title>Number of Questions Completed</Title>
-        <Value>120</Value> 
+      <Card background={cardColors[0]} textColor="#000000">
+        <Title textColor="#000000">Questions Completed</Title>
+        <Value textColor="#000000">120</Value> 
       </Card>
 
-      <Card>
-        <Title>Percentage Correct</Title>
+      <Card background={cardColors[4]} textColor="#FFFFFF">
+        <Title textColor="#FFFFFF">Percentage Correct</Title>
         <ResponsiveContainer width="100%" height="100%">
-          <LineChart data={[{name: 'Jan', percentage: 70}, {name: 'Feb', percentage: 80}, {name: 'Mar', percentage: 90}, {name: 'Apr', percentage: 75}, {name: 'May', percentage: 85}, {name: 'Jun', percentage: 95}]}>
-            <Line type="monotone" dataKey="percentage" stroke="#8884d8" />
-            <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="name" />
-            <YAxis />
+          <LineChart data={[{name: 'Jan', percentage: 70}, {name: 'Feb', percentage: 80}, {name: 'Mar', percentage: 90}, {name: 'Apr', percentage: 75}, {name: 'Jun', percentage: 95}]}>
+            <Line type="monotone" dataKey="percentage" stroke="#FFFFFF" />
+            <CartesianGrid stroke="#FFFFFF" />
+            <XAxis dataKey="name" stroke="#FFFFFF" /> {/* Added stroke prop here */}
+            <YAxis stroke="#FFFFFF" /> {/* Added stroke prop here */}
             <Tooltip />
           </LineChart>
         </ResponsiveContainer>
       </Card>
 
       <Card>
-        <Title>Area to Focus on</Title>
+        <Title textColor="#000000">Split 1 hour like this</Title>
         <ResponsiveContainer width="100%" height="100%">
           <PieChart>
-            <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8">
+            <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={75} fill="#8884d8">
               {
                 data.map((entry, index) => <Cell key={`cell-${index}`} fill={["#0088FE", "#00C49F", "#FFBB28", "#FF8042"][index % 4]}/>)
               }
@@ -88,19 +92,19 @@ function ProgressPage() {
         </ResponsiveContainer>
       </Card>
 
-      <Card>
-        <Title>Best Subject</Title>
-        <Value>Math</Value>
+      <Card background={cardColors[1]} textColor = "#000000">
+        <Title textColor="#000000">Best Subject</Title>
+        <Value textColor="#000000">Math</Value>
       </Card>
 
-      <Card>
-        <Title>Avg Time/Question</Title>
-        <Value>30 sec</Value>
+      <Card background={cardColors[2]} textColor="#FFFFFF">
+        <Title textColor="#FFFFFF">Avg. Time per Question</Title>
+        <Value textColor="#FFFFFF">30 sec</Value>
       </Card>
 
-      <Card>
-        <Title>Total Points</Title>
-        <Value>1000</Value>
+      <Card background={cardColors[3]} textColor="#FFFFFF">
+        <Title textColor="#FFFFFF">Total Points</Title>
+        <Value textColor="#FFFFFF">1000</Value>
       </Card>
       </CardContainer>
     </div>
